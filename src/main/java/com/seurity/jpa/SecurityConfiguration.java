65b2bec7f66e0a5152a42f763ada38e0b10dd03f
aditya.saraf <adityasaraf87@gmail.com>
2020-01-23 11:39:47 +0530

    Commit Spring Security JPA

---
+++ src/main/java/com/seurity/jpa/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.seurity.jpa;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	{
+		super.configure(auth);
+	}
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity security) throws Exception {
+		security.authorizeRequests()
+		.antMatchers("/admin").hasRole("ADMIN")
+		.antMatchers("/user").hasAnyRole("USER","ADMIN")
+		.antMatchers("/").permitAll()
+		.and().formLogin();
+	}
+	
+}
